8f5e831162e5edc5eb89408f8a3448aa58554518
Lúcio Andrade <lucioandradejr@gmail.com>
2024-07-01 12:20:47 -0400

    initial commit ✨

---
+++ cmd/api/api.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"store-api/service/user"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{addr, db}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
+
+	log.Println("Listening", s.addr)
+
+	return http.ListenAndServe(s.addr, router)
+}
